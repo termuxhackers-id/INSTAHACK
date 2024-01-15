edb18a9aba40bf4ee7f395a49c43a8974d7bd588
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2024-01-15 23:18:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo -e "[ installer tool for instahack @v2.0.5 - by Termux Hackers ID ]\n"
-echo -e "[wait] running pkg update and pkg upgrade (📦) ..."
+echo -e "\n[ installer tool for instahack @v2.0.5 - by Termux Hackers ID ]\n"
+echo -e "\n[wait] running pkg update and pkg upgrade (📦) ...\n"
 pkg update -y && pkg upgrade -y
-echo -e "[info] installing package and dependencies ..."
+echo -e "\n[info] installing package and dependencies ...\n"
 pkg install git which binutils python python-pip -y
-echo -e "[info] cloning github repository -> 'instahack' ..."
+echo -e "\n[info] cloning github repository -> 'instahack' ...\n"
 git clone https://github.com/termuxhackers-id/instahack && cd instahack
-echo -e "[info] installing module and requirements ..."
+echo -e "\n[info] installing module and requirements ...\n"
 pip install wheel && pip install rich pytz PyNacl requests licensing pycryptodomex phonenumbers httpagentparser
-echo -e "[info] remove README.md from instahack directories ..."
+echo -e "\n[info] remove README.md from instahack directories ...\n"
 find . -name '*.md' -delete
-echo -e "[done] running 'ihack.py' to open instahack console ..."
+echo -e "\n[done] running 'ihack.py' to open instahack console ...\n"
 python3 ihack.py
