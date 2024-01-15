89950fb38d90592f10a4dee6b01d17b641287cf3
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2024-01-15 23:07:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
+echo -e "[ installer tool for instahack @v2.0.5 - by Termux Hackers ID ]\n"
+echo -e "[wait] running pkg update and pkg upgrade (📦) ..."
 pkg update -y && pkg upgrade -y
+echo -e "[info] installing package and dependencies ..."
 pkg install git which binutils python python-pip -y
+echo -e "[info] cloning github repository -> 'instahack' ..."
 git clone https://github.com/termuxhackers-id/instahack && cd instahack
+echo -e "[info] installing module and requirements ..."
 pip install wheel && pip install rich pytz PyNacl requests licensing pycryptodomex phonenumbers httpagentparser
+echo -e "[info] remove README.md from instahack directories ..."
 find . -name '*.md' -delete
+echo -e "[done] running 'ihack.py' to open instahack console ..."
 python3 ihack.py
