6d00ef80cd9a57995f4883dbb32a1782cc41ab95
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2024-01-15 22:45:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 pkg update -y && pkg upgrade -y
 pkg install git which binutils python python-pip -y
-cd && git clone https://github.com/termuxhackers-id/instahack && cd instahack
+git clone https://github.com/termuxhackers-id/instahack && cd instahack
 pip install wheel && pip install rich pytz PyNacl requests licensing pycryptodomex phonenumbers httpagentparser
 find . -name '*.md' -delete
 python3 ihack.py
