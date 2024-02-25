de0c9ca65d0a833889d3b404ecdacd1978cf393f
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2024-02-25 21:03:54 +0700

    Update for ihack@v2.0.7
    Shell script to install instahack on Termux Android

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo -e "\n[ installer tool for instahack @v2.0.5 - by Termux Hackers ID ]\n"
+echo -e "\n[ installer tool for instahack @v2.0.7 - by Termux Hackers ID ]\n"
 echo -e "\n[wait] running pkg update and pkg upgrade (📦) ...\n"
 pkg update -y && pkg upgrade -y
 echo -e "\n[info] installing package and dependencies ...\n"
@@ -7,8 +7,8 @@ pkg install git which binutils python python-pip -y
 echo -e "\n[info] cloning github repository -> 'instahack' ...\n"
 git clone https://github.com/termuxhackers-id/instahack && cd instahack
 echo -e "\n[info] installing module and requirements ...\n"
-pip install wheel && pip install rich pytz PyNacl requests licensing pycryptodomex phonenumbers httpagentparser
-echo -e "\n[info] remove README.md from instahack directories ...\n"
-find . -name '*.md' -delete
+pip install wheel && pip install -r requirements.txt
+echo -e "\n[info] remove all README.md from data directories ...\n"
+find data/ -name '*.md' -delete
 echo -e "\n[done] running 'ihack.py' to open instahack console ...\n"
 python3 ihack.py
