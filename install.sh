b1595adf437dec4fba81271b1c6ff643b99143db
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2024-05-13 19:20:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo -e "\n[ installer tool for instahack @v2.0.7 - by Termux Hackers ID ]\n"
-echo -e "\n[wait] running pkg update and pkg upgrade (📦) ...\n"
+echo -e "\n[ installer tool for instahack @v2.0.9 - by Termux Hackers ID ]\n"
+echo -e "\n[wait] running pkg update and pkg upgrade ...\n"
 pkg update -y && pkg upgrade -y
+echo -e "\n[info] installing new repository: tur-repo ...\n"
+pkg install tur-repo -y && pkg update -y
 echo -e "\n[info] installing package and dependencies ...\n"
-pkg install git which binutils python python-pip -y
+pkg install git nano binutils build-essential python3.10 -y
+echo 'alias python3="python3.10"' >> $PREFIX/etc/bash.bashrc
+echo 'alias pip="python3.10 -m pip"' >> $PREFIX/etc/bash.bashrc
 echo -e "\n[info] cloning github repository -> 'instahack' ...\n"
 git clone https://github.com/termuxhackers-id/instahack && cd instahack
 echo -e "\n[info] installing module and requirements ...\n"
-pip install wheel && pip install -r requirements.txt
+pip install --upgrade pip && pip install wheel && pip install -r requirements.txt
 echo -e "\n[info] remove all README.md from data directories ...\n"
 find data/ -name '*.md' -delete
 echo -e "\n[done] running 'ihack.py' to open instahack console ...\n"
