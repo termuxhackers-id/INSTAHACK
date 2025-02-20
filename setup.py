99f3964f86f452531375640d5602472126453cce
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2025-02-21 00:08:25 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ihack',
-    version='3.0.5',
+    version='3.0.6',
     author='Termux Hackers ID (@iqbalmh18)',
     author_email='termuxhackers.id@gmail.com',
     description='A python based tool for hacking instagram accounts',
